6692b835bea782c1a093d0588b3e8c46adcc206d
talbenzur <tbz1996@gmail.com>
2022-12-07 22:32:37 +0200

    humdity selector- time and date website

--- main.go
+++ main.go
@@ -1,11 +1,55 @@
+//go get github.com/PuerkitoBio/goquery
+
 package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"strings"
 
-	"github.com/gocolly/colly"
+	"github.com/PuerkitoBio/goquery"
 )
 
+func main() {
+
+    webPage := "https://www.timeanddate.com/weather/israel/jerusalem"
+    resp, err := http.Get(webPage)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer resp.Body.Close()
+
+    if resp.StatusCode != 200 {
+        log.Fatalf("failed to fetch data: %d %s", resp.StatusCode, resp.Status)
+    }
+
+    doc, err := goquery.NewDocumentFromReader(resp.Body)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    //title := doc.Find("title").Text()
+    //fmt.Println(title)
+
+    humiditySelector:= "table.table--left.table--inner-borders-rows tbody tr:nth-child(6) td:nth-child(2)"
+    
+    words := doc.Find(humiditySelector).Map(func(i int, sel *goquery.Selection) string {
+        return fmt.Sprintf("%d: %s", i+1, sel.Text())
+    })
+    myword:=strings.Split( words[0:1][0], " ")
+    fmt.Println(myword[1][:len(myword[1])-1])
+
+    match := sampleRegexp.MatchString("[1-9]{2,3}%")
+
+}
+
+//table table--left table--inner-borders-rows
+
+/*
 func main() {
 
 	// regex1 := ">Rain<.*PercentageValue\">(.*)%" //
@@ -25,3 +69,4 @@ func main() {
 	c.Visit("https://weather.com/weather/today/l/1113fba38e09773bd1c20c1f70b48f045cd0d1bd62eaa2d37a47c5cc58d12b7b")
 
 }
+*/
