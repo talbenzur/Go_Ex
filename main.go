b3e9d03c2821b25bf129d2e39d3867840e182f49
SarayShlomi <saraysara1996@gmail.com>
2022-12-07 17:06:27 +0200

    stam

--- main.go
+++ main.go
@@ -6,20 +6,22 @@ import (
 	"github.com/gocolly/colly"
 )
 
-
-
 func main() {
 
-    regex1:= ">Rain<.*PercentageValue\">(.*)%" //https://weather.com/weather/tenday/l/cca0801d9062db761eb0521ed6bf5549ed4c546211046d09b9ac7ad09a6c556d#detailIndex5
+	// regex1 := ">Rain<.*PercentageValue\">(.*)%" //
+	// fmt.Println(regex1)
 
 	c := colly.NewCollector(
-		colly.AllowedDomains("en.wikipedia.org"),
+		colly.AllowedDomains("https://weather.com/?Goto=Redirected"),
 	)
-    // Find and print all links
-    c.OnHTML(".mw-parser-output", func(e *colly.HTMLElement) {
-        links := e.ChildAttrs("a", "href")
-        fmt.Println(links)
-    })
-    c.Visit("https://en.wikipedia.org/wiki/Web_scraping")
+	// Find and print all links
+	c.OnHTML("Precip", func(e *colly.HTMLElement) {
+		links := e.ChildAttrs("PercentageValue", "data-testid")
+		fmt.Println(links)
+		rainChance := e.ChildText("#PercentageValue")
+		fmt.Println("rainChance", rainChance)
+
+	})
+	c.Visit("https://weather.com/weather/today/l/1113fba38e09773bd1c20c1f70b48f045cd0d1bd62eaa2d37a47c5cc58d12b7b")
 
 }
